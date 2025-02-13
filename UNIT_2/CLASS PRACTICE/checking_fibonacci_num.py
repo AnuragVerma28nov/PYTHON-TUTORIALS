23e256725619e5cfe9efa72d357477efb49f29aa
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-02-13 19:02:20 +0530

    Add files via upload

---
+++ UNIT_2/CLASS PRACTICE/checking_fibonacci_num.py
@@ -0,0 +1,14 @@
+#Checking Fibonacci Number without using function
+num = int(input("Enter a number: "))
+a = 0
+b = 1
+while b < num:
+    temp = a + b
+    a = b
+    b = temp
+if b == num:
+    print(num, "is a Fibonacci number.")
+else:
+    print(num, "is not a Fibonacci number.")
+
+
---
+++ UNIT_2/CLASS PRACTICE/pattern_3.py
@@ -0,0 +1,21 @@
+''' pattern
+*
+**
+***
+'''
+
+for i in range(1, 4):
+    print("*" * i)
+print("\n")
+'''
+*
+**
+***
+****
+***** '''
+for i in range(1, 6):
+    print("*" * i)    
+
+
+
+
