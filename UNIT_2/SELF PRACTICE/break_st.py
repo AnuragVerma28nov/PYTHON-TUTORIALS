b012ba2fe9792e750b20aff15b4113bcada59362
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-02-16 20:30:38 +0530

    Add files via upload

---
+++ UNIT_2/SELF PRACTICE/break_st.py
@@ -0,0 +1,7 @@
+# break statement
+n=0
+while n<10:
+    if n==6: 
+        break
+    print(n)
+    n=n+1 
---
+++ UNIT_2/SELF PRACTICE/continue_st.py
@@ -0,0 +1,7 @@
+# continue statement
+n=0
+while n<10:
+    n=n+1
+    if n==5:
+        continue
+    print(n)
