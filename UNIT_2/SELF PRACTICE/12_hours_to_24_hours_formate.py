f9f0b5295ea3a12976a0be210d511c854b08bd20
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-02-16 22:56:20 +0530

    Add files via upload

---
+++ UNIT_2/SELF PRACTICE/12_hours_to_24_hours_formate.py
@@ -0,0 +1,12 @@
+#  converts a 12-hour time format to a 24-hour time format without using a function:
+time_12hr = input("Enter time in 12-hour format (HH:MM AM/PM): ") # Input
+time, period = time_12hr.split() # Extract  AM/PM part
+hours, minutes = map(int, time.split(':'))
+if period.upper() == "AM": # Convert to 24-hour format
+    if hours == 12:
+        hours = 0
+elif period.upper() == "PM":
+    if hours != 12:
+        hours += 12 
+time_24hr = f"{hours:02}:{minutes:02}"
+print("Time in 24-hour format:", time_24hr)
---
+++ UNIT_2/SELF PRACTICE/checking_fibonacci_num.py
@@ -0,0 +1,13 @@
+#checking Fibonacci Number using function
+num = int(input("Enter a number: "))
+a = 0
+b = 1
+while b < num:
+    temp = a + b
+    a = b
+    b = temp
+if b == num:
+    print(num, "is a Fibonacci number.")
+else:
+    print(num, "is not a Fibonacci number.")
+ 
