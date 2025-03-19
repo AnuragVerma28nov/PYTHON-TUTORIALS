b54fac1278c120a99444b83e356218649fb74273
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-03-19 21:51:58 +0530

    Add files via upload

---
+++ UNIT_2/SELF PRACTICE/use_of_range.py
@@ -0,0 +1,14 @@
+# use of range function
+
+for i in range(10): # range(stop)
+    print(i,end=" ")
+    
+print("\n")   
+
+for i in range(1,10): # range(start,stop)
+    print(i,end=" ")
+    
+print("\n")
+   
+for i in range(1,10,2): # range(start,stop,step)
+    print(i,end=" ")
