88230fcda8e58230469e2965396e8820dbdb364e
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-03-19 22:01:51 +0530

    Update fibonnic_series.py

--- UNIT_2/SELF PRACTICE/fibonnic_series.py
+++ UNIT_2/SELF PRACTICE/fibonnic_series.py
@@ -2,7 +2,7 @@
 n_term=int(input("Eneter number of terms: "))
 n_1=0
 n_2=1
-if n_term<0:
+if n_term<=0:
     print("please enter a postive number: ")
 elif n_term==1:
     print("fibonnic series: ",n_1)  
