f0bb7ca51ce7516cd93ca6bb01919526a682087d
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-03-20 10:57:52 +0530

    Add files via upload

---
+++ UNIT_5/SELF PRACTICE/creating_package/my_packages/using_module.py
@@ -0,0 +1,4 @@
+from .module_1 import greet
+from .module_2 import add
+print(greet("anurag"))
+print(add(10,20))
---
+++ UNIT_5/SELF PRACTICE/creating_package/using_packages.py
@@ -0,0 +1,4 @@
+import my_packages
+
+print(my_packages.greet("anurag"))
+print(my_packages.add(10,20))
