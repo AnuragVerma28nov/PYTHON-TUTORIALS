c73ab3532a882eab5fcc9382f4680c3e74b01a90
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-04-10 11:30:41 +0530

    Delete UNIT_1 directory

--- UNIT_1/CLASS PRACTICE/main.py
+++
@@ -1,5 +0,0 @@
-# my frist program 
-
-print("I AM ANURAG. I AM TRYING TO LEARN PYTHON PROGRAMMINNG.")
-print(" HELLO WORLD!")
-print("HELLO PYTHON")
--- UNIT_1/SELF PRACTICE/printing_ascii_value.py
+++
@@ -1,4 +0,0 @@
-# printing ascii value
-char = input("Enter a character: ")
-ascii = ord(char)
-print("The ASCII value of", char, "is:", ascii)                                                                                                                                                                                                                                                                                                                                 
