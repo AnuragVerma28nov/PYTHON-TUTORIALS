2c81f66f94c120598edb9881e9e8a03b38f27faf
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-08-03 22:35:25 +0530

    Add files via upload

---
+++ STUDY MATERIALS/CH_1/Prog_2.py
@@ -0,0 +1,15 @@
+# Poem: Twinkle, Twinkle, Little Star
+
+print("Twinkle, twinkle, little star,")
+print("How I wonder what you are!")
+print("Up above the world so high,")
+print("Like a diamond in the sky.")
+
+
+# Output:
+# Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+
