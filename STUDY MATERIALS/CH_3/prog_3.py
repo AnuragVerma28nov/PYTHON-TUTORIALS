bff93df2969de8598d38118e7ff182eca6658874
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-08-04 22:54:12 +0530

    Add files via upload

---
+++ STUDY MATERIALS/CH_3/prog_3.py
@@ -0,0 +1,20 @@
+# checking type of variables assigned
+
+n = "Anurag"
+print(type(n))
+
+n = 2
+print(type(n))
+
+n =2.5
+print(type(n))
+
+n ={2,2,2,"anu"}
+print(type(n))
+
+# Output:
+
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'set'>
