a3eb3fab2095cce717e6f511f5607d2a8de27ce8
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-08-14 13:29:51 +0530

    Add files via upload

---
+++ STUDY MATERIALS/CH_4/prog_1.py
@@ -0,0 +1,10 @@
+# Display a user entered name followed by good morning using input() function
+
+sr =(input("Enter your name: "))
+
+print("Good Morning,",st)
+
+# Output: 
+
+# Enter your name: Anurag
+# Good Morning, Anurag
