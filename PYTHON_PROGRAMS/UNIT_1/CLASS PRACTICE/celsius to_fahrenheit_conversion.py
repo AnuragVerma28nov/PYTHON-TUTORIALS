2e9dbc04ee8c385688ff714159ac468201c191c6
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-09-13 20:22:44 +0530

    Update celsius to_fahrenheit_conversion.py

--- PYTHON_PROGRAMS/UNIT_1/CLASS PRACTICE/celsius to_fahrenheit_conversion.py
+++ PYTHON_PROGRAMS/UNIT_1/CLASS PRACTICE/celsius to_fahrenheit_conversion.py
@@ -1,4 +1,5 @@
 # Celsius to Fahrenheit Conversion
 cel = float(input("Enter temperature in Celsius: "))
 fah = (cel * 9/5) + 32
-print(f"{cel}°C is equal to {fah}°F")
+
+print(f"{cel}°C is equal to {fah}°F"
