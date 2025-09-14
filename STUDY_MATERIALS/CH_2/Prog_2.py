560d699fa03d3489db2d3310580857cff6fa1a48
ANURAG VERMA <anuragverma28nov@gmail.com>
2025-09-14 23:04:13 +0530

    Update Prog_2.py

--- STUDY_MATERIALS/CH_2/Prog_2.py
+++ STUDY_MATERIALS/CH_2/Prog_2.py
@@ -3,7 +3,7 @@
 print("Twinkle, twinkle, little star,")
 print("How I wonder what you are!")
 print("Up above the world so high,")
-print("Like a diamond in the sky."
+print("Like a diamond in the sky.")
 
 
 # Output:
